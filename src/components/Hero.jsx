246913cf38faa905519903a1cada570012b2294b
Ryavnn <ryanisnjoroge@gmail.com>
2025-06-25 23:53:38 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,25 +6,28 @@ const Hero = () => {
 
   return (
     <>
-
-      <div className="w-full h-[100vh] bg-[url(/assets/background.png)] flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
+      <div className="w-full md:h-[100vh] h-[70vh] bg-[url(/assets/background.png)] flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
         <Navbar />
         <div className="h-full w-full flex justify-center item-center  text-white ">
-          <div className="w-[90%] h-[90%] p-5 flex flex-col justify-center  gap-5 rounded-lg ">
-            <h1 className="text-lg text-green-400 pl-3">Hi, my name is</h1>
-            <h2 className="text-8xl font-extrabold text-gray-500">
+          <div className="w-[90%] h-[90%] p-5 flex flex-col justify-center items-center md:items-start  gap-5 rounded-lg text-center">
+            <h1 className="md:text-lg text-green-400 pl-3 text-[1.5rem]">Hi, my name is</h1>
+            <h2 className="md:text-8xl font-extrabold text-gray-500 text-5xl">
               Ryan Njoroge
             </h2>
-            <h3 className="text-7xl font-extrabold text-gray-400">
+            <h3 className="md:text-7xl font-extrabold text-gray-400 text-4xl">
               I build things for the web
             </h3>
-            <p className="text-gray-300 text- w-100">
+            <p className="text-gray-300 text- md:w-100 text-[1.2rem]">
               I'm a fronted developer specializing in building and even
               designing expectational digital expirience.
             </p>
-            <button className="px-1 w-50 h-10 border border-green-500 text-green-500 hover:bg-green-500 hover:text-white rounded-sm">
+            <a
+              href="/resume.pdf"
+              download
+              className="px-4 w-[200px] h-10 flex items-center justify-center border border-green-500 text-green-500 hover:bg-green-500 hover:text-white rounded-sm transition"
+            >
               Download Resume
-            </button>
+            </a>
           </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@
 const Navbar = () => {
   return (
     <>
-    <div className='w-full md:w-[700px] h-20 bg-white/10 mt-4 rounded-md flex justify-between items-center px-5'>
+    <div className='w-[90%] md:w-[700px] h-20 bg-white/10 mt-4 rounded-md flex justify-between items-center px-5'>
     <h1 className='text-xl text-white font-extrabold hidden md:flex'>Ryan Njoroge</h1>
     <div className='w-full md:w-[65%] flex'>
         <ul className='flex justify-evenly md:justify-between items-center w-full text-white text-lg'>
-            <li>Tech-stack</li>
+            <li>About</li>
             <li>Services</li>
             <li>Projects</li>
             <li className='bg-green-500 text-white h-10 rounded-md p-2 w-30 text-center hidden md:block'>Get in touch</li>
