62df59e21933803fbdd00ac7be5d99848a7258bf
Ryavnn <ryanisnjoroge@gmail.com>
2025-07-05 09:55:47 +0300

    Update Hero and Navbar components for improved styling; remove global CSS reset

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,14 @@ const Hero = () => {
 
   return (
     <>
-      <div className="w-full md:h-[100vh] h-[70vh] bg-[url(/assets/background.png)] flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
+      <div className="w-full md:h-[100vh] h-[70vh] bg-[#0a192f] flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
         <Navbar />
         <div className="h-full w-full flex justify-center item-center  text-white ">
           <div className="w-[90%] h-[90%] p-5 flex flex-col justify-center items-center md:items-start  gap-5 rounded-lg text-center">
-            <h1 className="md:text-lg text-green-400 pl-3 text-[1.5rem]">Hi, my name is</h1>
-            <h2 className="md:text-8xl font-extrabold text-gray-500 text-5xl">
+            <h1 className="md:text-lg text-[#64ffda] pl-3 text-[1.5rem]">
+              Hi, my name is
+            </h1>
+            <h2 className="md:text-8xl font-extrabold text-White text-5xl">
               Ryan Njoroge
             </h2>
             <h3 className="md:text-7xl font-extrabold text-gray-400 text-4xl">
@@ -24,7 +26,7 @@ const Hero = () => {
             <a
               href="/resume.pdf"
               download
-              className="px-4 w-[200px] h-10 flex items-center justify-center border border-green-500 text-green-500 hover:bg-green-500 hover:text-white rounded-sm transition"
+              className="px-4 w-[200px] h-10 flex items-center justify-center border border-[#64ffda] text-[#64ffda] hover:bg-[#64ffda] hover:text-white rounded-sm transition"
             >
               Download Resume
             </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,23 @@
 const Navbar = () => {
   return (
     <>
-    <div className='w-[90%] md:w-[700px] h-20 bg-white/10 mt-4 rounded-md flex justify-between items-center px-5'>
-    <h1 className='text-xl text-white font-extrabold hidden md:flex'>Ryan Njoroge</h1>
-    <div className='w-full md:w-[65%] flex'>
-        <ul className='flex justify-evenly md:justify-between items-center w-full text-white text-lg'>
-            <li>About</li>
-            <li>Services</li>
-            <li>Projects</li>
-            <li className='bg-green-500 text-white h-10 rounded-md p-2 w-30 text-center hidden md:block'>Get in touch</li>
-        </ul>
-    </div>
-    </div>
+      <div className="w-[90%] md:w-[700px] h-20 bg-white/10 m-2 rounded-md flex justify-between items-center px-5">
+        <h1 className="text-xl text-white font-extrabold hidden md:flex">
+          Njoroge.dev
+        </h1>
+        <div className="w-full md:w-[65%] flex">
+          <ul className="flex justify-evenly md:justify-between items-center w-full text-white text-lg">
+            <li className="cursor-pointer">About</li>
+            <li className="cursor-pointer">Services</li>
+            <li className="cursor-pointer">Projects</li>
+            <li className="bg-[#64ffda] text-gray-500 h-10 rounded-md p-1 pl-1 w-30 text-center hidden md:flex cursor-pointer hover:border hover:border-[#64ffda] hover:bg-transparent">
+              Get in touch
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Navbar
