fb7134d79473bf9662072da4b8ddd3c33067a0ef
Ryavnn <ryanisnjoroge@gmail.com>
2025-07-05 16:45:40 +0300

    Add Formspree integration to ContactUs component for form handling and submission feedback

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
-
+import { useForm, ValidationError } from '@formspree/react';
 const ContactUs = () => {
+    const [state, handleSubmit] = useForm("xkgbajaw");
+    if (state.succeeded) {
+        return (
+          <div className="h-screen flex items-center justify-center bg-[#0a192f]">
+            <p className="text-white text-xl font-semibold">Thanks for your submission! 👋</p>
+          </div>
+        );
+    }
   return (
     <div className="w-full h-screen flex justify-center items-center ">
       <div className="w-[90%] h-[90%] p-5 flex justify-center items-center bg-[#8892b0] rounded-xl">
@@ -11,26 +19,45 @@ const ContactUs = () => {
             idea, I'm just an email away. Let's connect!
           </p>
           <div>
-            <form className="flex flex-col gap-3">
+            <form className="flex flex-col gap-3" onSubmit={handleSubmit} method='POST'>
               <label className="text-gray-700 ">Name</label>
               <input
                 type="text"
+                name='name'
                 placeholder="John Doe"
                 className="p-2 rounded-md bg-amber-50 outline-none focus:outline-none"
+              />
+              <ValidationError
+                prefix="Name"
+                field="name"
+                errors={state.errors}
               />
               <label className="text-gray-700">Email</label>
               <input
                 type="email"
                 placeholder="johndoe@gmail.com"
+                name='email'
                 className="p-2 rounded-md bg-amber-50 outline-none focus:outline-none"
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
               />
               <label className="text-gray-700">Message</label>
               <textarea
                 placeholder="Enter message here..."
                 className="p-2 rounded-md h-32 bg-amber-50 outline-none focus:outline-none"
+                name='message'
               ></textarea>
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
               <button
                 type="submit"
+                disabled={state.submitting}
                 className="bg-[#0a192f] text-white p-2 rounded-md hover:bg-[#64ffda] hover:text-gray-900 transition"
               >
                 Submit
