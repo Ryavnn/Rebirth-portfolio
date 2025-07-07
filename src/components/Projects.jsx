26d506629bfe1af3ee86491525e9243e1ceb8e87
Ryavnn <ryanisnjoroge@gmail.com>
2025-07-07 13:52:38 +0300

    Update project descriptions in Projects component for clarity and detail

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
       {
         title: "Restaurant Management System",
         description:
-          "Description of project 1 goes here. It was built using React and Flask.",
+          "A full-stack restaurant management app with POS, kitchen display, manager dashboard, client-facing order tracking, and receipt generation. Improved efficiency, reduced order confusion, and streamlined operations across roles.",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi88XhdL4bEyh-d6N1gndDZq-qrGLd8uALZQ&s",
         techStack: ["React", "Tailwind", "Flask", "PostgreSQL"],
@@ -14,7 +14,7 @@ const Projects = () => {
       {
         title: "Flavour Fusion",
         description:
-          "Description of project 2 goes here. It was built using React and Flask.",
+          "FlavourFusion is a sleek, responsive recipe discovery platform built using React and Tailwind CSS. The site is optimized for performance and built with scalable, reusable components, making it easy to expand into features like meal planning and community contributions.",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi88XhdL4bEyh-d6N1gndDZq-qrGLd8uALZQ&s",
         techStack: ["React", "Tailwind"],
@@ -23,14 +23,14 @@ const Projects = () => {
       {
         title: "Conven Build Blueprint",
         description:
-          "Description of project 3 goes here. It was built using React and Flask.",
+          "ConvenBuild, a construction company site targeting private and government clients. Used React, Tailwind CSS, and shadcn/ui to create a clean, modern layout with strong CTAs, service sections, and portfolio previews. Focused on performance, accessibility, and scalable components for future features like quote forms and case studies.",
         image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi88XhdL4bEyh-d6N1gndDZq-qrGLd8uALZQ&s",
-        techStack: ["React", "Tailwind", "Flask", "PostgreSQL"],
+        techStack: ["React", "Tailwind", "ShadcnUI"],
         link: "https://conven-build-blueprint.vercel.app/",
       },
       {
-        title: "Restaurant Management System",
+        title: "Employee Management System",
         description:
           "Description of project 1 goes here. It was built using React and Flask.",
         image:
@@ -47,7 +47,7 @@ const Projects = () => {
       {projectData.map((project, index) => (
         <div
           key={index}
-          className="w-full md:w-[300px] h-80 shadow-xl flex flex-col items-center justify-center border border-white/20 rounded-xl"
+          className="w-full md:w-[300px] min-h-80 shadow-xl flex flex-col items-center justify-center border border-white/20 rounded-xl"
         >
           <div className="w-full h-1/2 bg-gray-300 rounded-t-lg flex justify-center items-center">
             <img
@@ -57,14 +57,18 @@ const Projects = () => {
             />
           </div>
           <div className="w-full h-1/2 p-2 text-gray-700">
-            <ul className="flex justify-between items-center w-full h-10 p-2 text-white gap-1">
+            <h3 className="text-lg font-semibold mb-2 text-gray-300">
+              {project.title}
+            </h3>
+            <ul className="flex items-center w-full h-10 p-2 text-white gap-1">
               {project.techStack.map((tech, i) => (
                 <li key={i} className="tech-stack">
                   {tech}
                 </li>
               ))}
             </ul>
-            <p className="text-sm mb-3">{project.description}</p>
+
+            <p className="text-sm mb-3 text-gray-300">{project.description}</p>
           </div>
           <a
             href={project.link}
